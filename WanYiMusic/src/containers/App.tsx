fb8a5ba003055aeb7add80732ded07d2979f5f42
advanceCabbage <yanghao18328@163.com>
2020-05-29 00:13:06 +0800

    全局引入antd

--- WanYiMusic/src/containers/App.tsx
+++ WanYiMusic/src/containers/App.tsx
@@ -1,4 +1,5 @@
 import * as  React from 'react';
+import './App.css'
 export default class APP extends React.Component{
   constructor(props:any){
     super(props)
--- WanYiMusic/src/views/A.tsx
+++ WanYiMusic/src/views/A.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 // import {Button} from 'antd'
+import Button from 'antd/lib/button'
 export default class A extends React.Component{
     render(){
         return(<div>
             我是页面A
-            {/* <Button>按钮</Button> */}
+            <Button type="primary">按钮</Button>
             <div>
                 
             </div>
