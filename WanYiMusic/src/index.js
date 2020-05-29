2f1f9cdc23e89a5c02bc95f99177a9cb9add3f37
yanghao02 <yanghao02@sobey.com>
2020-05-29 18:49:36 +0800

    antd按需引入

--- WanYiMusic/src/index.js
+++ WanYiMusic/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './routes/index'
 import {Router,hashHistory } from 'react-router'
+import {Provider } from 'react-redux'
 import store from './store/index.ts'
 ReactDOM.render((
-  <Router store={store}  history={hashHistory} routes={routes}></Router>
+  <Router store={store}   history={hashHistory} routes={routes}>
+      <Provider ></Provider>
+  </Router>
 ), document.getElementById('root'));
 
--- WanYiMusic/src/views/A.tsx
+++ WanYiMusic/src/views/A.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-// import {Button} from 'antd'
-import Button from 'antd/lib/button'
+import {Button} from 'antd'
+// import Button from 'antd/lib/button'
 export default class A extends React.Component{
     render(){
         return(<div>
